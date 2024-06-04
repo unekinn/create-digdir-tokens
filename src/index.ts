b6a6536e2c4b69283875531b6f50ddf693b0fe1c
Une Kinn Ekroll <une.kinn.ekroll@bekk.no>
2024-06-04 10:20:45 +0200

    Print "Next steps" instructions on completion

--- src/index.ts
+++ src/index.ts
@@ -134,7 +134,7 @@ async function main() {
     enter/return: Complete answer
 ${green("◉")}   Light ${dim("- This is the default mode, and cannot be disabled")}`,
         onState: (obj: { value: Choice[] }) => {
-          obj.value.push({ title: "Light", value: "Light", selected: true });
+          obj.value.unshift({ title: "Light", value: "Light", selected: true });
         },
       },
       {
@@ -288,6 +288,52 @@ Will now create the following:
     path.join(TARGET_DIR, "package.json"),
     JSON.stringify(packageJsonTemplate, undefined, 2)
   );
+
+  const themeModeCombinations = themes.flatMap((theme) =>
+    modes.map((mode): [theme: string, mode: string] => [theme, mode])
+  );
+
+  console.log(`
+🎉 Files successfully generated!
+
+${bold().underline("Next steps")}
+
+${bold("To use the tokens in Figma:")}
+1. Initialise a git repository in ${TARGET_DIR}
+2. Push the tokens to GitHub, GitLab or Azure DevOps
+3. Set up sync in Tokens Studio for Figma -
+   https://docs.tokens.studio/sync/sync
+4. Use the "Create variables" action in Tokens Studio -
+   https://docs.tokens.studio/variables/creating-variables
+5. Push the resulting variables from Tokens Studio to Git
+
+${bold(`Customizing the theme${themes.length > 1 ? "s" : ""}`)}
+1. Go to https://theme.designsystemet.no and set up a color theme
+2. Press "Kopier tema"
+3. Under "Json til Figma", copy the contents under ${modes.join(" / ")} to
+   the corresponding file under ${TOKENS_TARGET_DIR}:
+${themeModeCombinations.map(([theme, mode]) => `     ${bold(`${theme}, ${mode}`)}: primitives/colors/${normalizeTokenSetName(mode)}/${normalizeTokenSetName(theme)}.json`).join("\n")}
+   This can also be done in Tokens Studio for Figma.
+4. ${bold("IMPORTANT!")} In the JSON data you copied, replace "theme" on line 2
+   with the correct theme identifier, depending on the theme you're customizing.
+   This is the same as the json filename without extension (e.g. ${themes.map((x) => `"${normalizeTokenSetName(x)}"`).join(", ")}).
+${themes.length > 1 ? "5. Repeat steps 1—4 for the remaining themes" : ""}
+
+${bold("Use the theme in code:")}
+In ${TARGET_DIR}
+1. Check that the package.json file is set up to your liking
+2. npm run build ${dim(`- builds the css files for each theme and outputs them to ./dist`)}
+3. npm publish   ${dim(`- will publish the package to npm as ${packageName},
+                   unless you manually changed package.json`)}
+
+In a different npm package (e.g. a frontend web app), follow the "Get started"
+instructions on https://github.com/digdir/designsystemet but replace
+@digdir/designsystemet-theme with ${packageName}. E.g.
+- npm i ${packageName} @digdir/designsystemet-css @digdir/designsystemet-react
+- import '${packageName}';
+  import '@digdir/designsystemet-css';
+  import { Button } from '@digdir/designsystemet-react';
+`);
 }
 
 function isValidThemeName(themes: string[], value: string): true | string {
